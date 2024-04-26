ef2b414316d1fe3c6b664440d1197df930026a78
jigliPook <julzzr@gmail.com>
2024-04-26 12:09:27 +0300

    shell commands

---
+++ protopool.sh
@@ -0,0 +1,45 @@
+
+
+#Set git email
+git config --global user.email "julzzr@gmail.com"
+#set git username
+git config --global user.name "jigliPook"
+
+
+cd /Users/yuliaryv/Documents/git            
+git clone https://github.com/BenAssa123/ProtoFi.git
+
+
+cd ProtoFi
+touch new_test.txt
+
+#Use the git status command to see the changes. Your new file should appear as "untracked".
+git status
+
+#Add the new file to the staging area.
+#Use the git add command to add the new file. Replace filename with the name of your file.
+git add filename
+
+#If you want to add all new and changed files at once, you can use:
+git add .
+# ^ That's what I did
+
+#Commit the changes.
+#Use the git commit command to commit the added files to your local repository. Include a meaningful commit message.
+git commit -m "Add new file"
+
+
+# Optional pull Latest Changes
+# pull the latest changes from the remote repository. to avoid conflicts.
+#Replace "main" with whatever branch you're working on
+git pull origin main
+
+#Push the changes to the remote repository.
+#Use the git push command to push your local commits to the remote repository. If you are using the default branch (often named main or master), you would use:
+git push origin main
+xxn141yv18P9aSS
+# I created a token for this rep (use it for authentication):
+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
+
+# a more permanent solution to avoid authentication:
+git config --global credential.helper osxkeychain
